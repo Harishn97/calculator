b28716bab3d3db1b146acaad83c33bcbdf9559d6
Harishn97 <harishnaricchetty@gmail.com>
2021-03-23 15:46:07 -0400

    Feature- added square function

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -40,11 +40,11 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
 
-    # def test_square(self):
-    #     test_data = CsvReader('/src/csv/Unit Test Square.csv').data
-    #     for row in test_data:
-    #         self.assertEqual(self.calculator.square(row['Value 1']), float(row['Result']))
-    #         self.assertEqual(self.calculator.result, float(row['Result']))
+    def test_square(self):
+        test_data = CsvReader('/src/csv/Unit Test Square.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.square(row['Value 1']), float(row['Result']))
+            self.assertEqual(self.calculator.result, float(row['Result']))
 
     # def test_squareroot(self):
     #     test_data = CsvReader('/src/csv/Unit Test Square Root.csv').data
