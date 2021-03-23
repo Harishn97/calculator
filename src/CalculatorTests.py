3c648cab2edd70400aa78801b874f0b2f7582e32
Harishn97 <harishnaricchetty@gmail.com>
2021-03-23 15:44:03 -0400

    Feature- added division function

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -7,6 +7,7 @@ class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.calculator = Calculator()
+        CsvReader.data = []
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
@@ -33,11 +34,11 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
-    # def test_division(self):
-    #     test_data = CsvReader('/src/csv/Unit Test Division.csv').data
-    #     for row in test_data:
-    #         self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
-    #         self.assertEqual(self.calculator.result, float(row['Result']))
+    def test_division(self):
+        test_data = CsvReader('/src/csv/Unit Test Division.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
+            self.assertEqual(self.calculator.result, float(row['Result']))
 
     # def test_square(self):
     #     test_data = CsvReader('/src/csv/Unit Test Square.csv').data
