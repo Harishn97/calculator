a5969e04d3c3e3e37dca0fdfd3c68e6bd92c7b35
Harishn97 <harishnaricchetty@gmail.com>
2021-03-23 15:41:05 -0400

    Feature- added subtraction function

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -28,12 +28,12 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
-    # def test_multiplication(self):
-    #     test_data = CsvReader('/src/csv/Unit Test Multiplication.csv').data
-    #     for row in test_data:
-    #         self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
-    #         self.assertEqual(self.calculator.result, int(row['Result']))
-    #
+    def test_multiplication(self):
+        test_data = CsvReader('/src/csv/Unit Test Multiplication.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
     # def test_division(self):
     #     test_data = CsvReader('/src/csv/Unit Test Division.csv').data
     #     for row in test_data:
